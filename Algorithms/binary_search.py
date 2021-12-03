60c0e1508f81c7dd2fe40a1a1ffe514aac3270e4
breaze <simon.gaviriao@gmail.com>
2021-12-03 00:31:47 -0500

    Binary Search Updated

--- Algorithms/binary_search.py
+++ Algorithms/binary_search.py
@@ -1,7 +1,6 @@
 def binary_search(list, target):
     max = len(list)
     min = 0
-    i = 0
     while(min<len(list)-1 and max >0):
         target_position = round((max+min)/2)
         if list[target_position] == target:
@@ -10,9 +9,7 @@ def binary_search(list, target):
             min = target_position
         else:
             max = target_position
-        i = i + 1
     return -1
-
 
 list = list(range(0,100))
 target_position = binary_search(list=list, target=80)
