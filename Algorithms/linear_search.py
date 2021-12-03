88f70a49dcf00a18c070d86dac3d9cfd1eeb2ca0
breaze <simon.gaviriao@gmail.com>
2021-12-02 20:43:54 -0500

    Linear search algorithm

---
+++ Algorithms/linear_search.py
@@ -0,0 +1,9 @@
+def linear_search(list, target):
+    for i in range(len(list)):
+        if list[i] == target:
+            return i
+    return -1
+
+list = [30, 25, 2, 15, 29, 5, 4, 36]
+target_position = linear_search(list=list, target=59)
+print(target_position)
